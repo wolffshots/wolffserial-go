83f302d07abfc819726a89f9bb67a89671f5b23c
jadon <16850875+wolffshots@users.noreply.github.com>
2021-07-18 00:19:29 +0200

    added baseline functional content

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/wolffserial-go
+
+go 1.16
+
+require (
+	github.com/itchyny/volume-go v0.2.1
+	go.bug.st/serial v1.3.0
+)
---
+++ main.go
@@ -0,0 +1,85 @@
+package main
+
+import "log"
+import "fmt"
+import "strings"
+import "go.bug.st/serial"
+import "go.bug.st/serial/enumerator"
+
+// import "github.com/itchyny/volume-go"
+
+/**
+ * VOLUME
+ * get volume
+ *  vol, err := volume.GetVolume()
+ *  if err != nil {
+ *    log.Fatal(err)
+ *  }
+ * mute volume
+ *  volume.Mute()
+ * set volume - we'll use this to set to the adc value of the slider (as a perc of its max)
+ *  volume.SetVolume(50)
+ */
+
+func main() {
+	list()
+	mode := &serial.Mode{
+		BaudRate: 115200,
+	}
+	port, err := serial.Open("COM3", mode)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		cmd := strings.Trim(get_cmd(port), "\n")
+		fmt.Printf("cmd: %s\n", cmd)
+		if strings.EqualFold(strings.Trim(cmd, "\n "), "END") {
+			break
+		} else if strings.EqualFold(strings.Trim(cmd, "\n "), "B01") {
+
+		} else if strings.EqualFold(strings.Trim(cmd, "\n "), "B00") {
+
+		}
+	}
+	fmt.Println("end of prog")
+}
+
+func get_cmd(port serial.Port) string {
+	buff := make([]byte, 100)
+	cmd := ""
+	for {
+		// Reads up to 100 bytes
+		n, err := port.Read(buff)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if n == 0 {
+			break
+		}
+		cmd += string(buff[:n])
+		// If we receive a newline stop reading
+		if strings.Contains(string(buff[:n]), "\n") {
+			return string(cmd)
+		}
+	}
+	return "EOF"
+}
+
+func list() {
+	ports, err := enumerator.GetDetailedPortsList()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(ports) == 0 {
+		fmt.Println("No serial ports found!")
+		return
+	}
+	for _, port := range ports {
+		fmt.Printf("Found port: %s\n", port.Name)
+		if port.IsUSB {
+			fmt.Printf("   USB ID     %s:%s\n", port.VID, port.PID)
+			fmt.Printf("   USB serial %s\n", port.SerialNumber)
+		}
+	}
+}
